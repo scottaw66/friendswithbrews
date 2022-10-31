9c4723863dfa08cfff1af76375b660f8c1aaa5df
Scott Willsey <scott@saw66.com>
2022-10-30 20:13:22 -0700

    Handles all kinds of brews now (beer, coffee, tea, anything)

--- src/components/utilities/beernames.mjs
+++ src/components/utilities/beernames.mjs
@@ -1,10 +1,10 @@
-import beer from "../../data/beer.json" assert { type: "json" };
+import brew from "../../data/brew.json" assert { type: "json" };
 
-let beers = Array.from(beer).sort((a, b) => {
+let brews = Array.from(brew).sort((a, b) => {
   if (a.name < b.name) {
     return -1;
   } else {
     return 1;
   }
 });
-beers.map((beer) => console.log(`${beer.name} ${beer.id}`));
+brews.map((brew) => console.log(`${brew.name} ${brew.id}`));
