0db961f32e958b58df358445d414f39f03f9c2ea
Scott Willsey <scott@saw66.com>
2022-10-31 09:56:58 -0700

    Beer type with appropriate astro icon now in beer details view

--- src/components/utilities/stringformatter.mjs
+++ src/components/utilities/stringformatter.mjs
@@ -1,3 +1,16 @@
 export function capitalize(word) {
   return word.charAt(0).toUpperCase() + word.slice(1);
 }
+
+export function brewIcon(brewType) {
+  switch (brewType) {
+    case "beer":
+      return "ion:beer";
+    case "tea":
+      return "mdi:tea";
+    case "coffee":
+      return "fa-solid:coffee";
+    default:
+      return "ion:beer";
+  }
+}
