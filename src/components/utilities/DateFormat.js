4894d97ce4e758e1b39b31c5612edffa4e8bf472
Scott Willsey <sunny.flame0794@fastmail.com>
2023-02-06 13:57:54 -0800

    Fixes DateFormat utility

--- src/components/utilities/DateFormat.js
+++ src/components/utilities/DateFormat.js
@@ -1,7 +1,6 @@
-import {format} from "date-fns";
+import { format } from "date-fns";
 import enUS from "date-fns/locale/en-US/index.js";
-import pkg from "date-fns-tz";
-const {formatInTimeZone} = pkg;
+import { formatInTimeZone } from "date-fns-tz";
 
 const postPattern = "eeee, dd MMM yyyy";
 const tz = "America/Los_Angeles";
@@ -9,15 +8,15 @@ const tz = "America/Los_Angeles";
 export function rfc2822(date) {
   const pattern = "eee, dd MMM yyyy HH:mm:ss zzz";
 
-  return formatInTimeZone(date, tz, pattern, {locale: enUS});
+  return formatInTimeZone(date, tz, pattern, { locale: enUS });
 }
 
 export function postdate(date) {
-  return formatInTimeZone(date, tz, postPattern, {locale: enUS});
+  return formatInTimeZone(date, tz, postPattern, { locale: enUS });
 }
 
 export function utcdate(date) {
-  return formatInTimeZone(date, "UTC", postPattern, {locale: enUS});
+  return formatInTimeZone(date, "UTC", postPattern, { locale: enUS });
 }
 
 export function year() {
