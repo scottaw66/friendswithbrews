7938af8ad27d1d1a2103d83b39921347901ebd0b
Scott Willsey <sunny.flame0794@fastmail.com>
2023-02-22 01:21:06 -0800

    Updates ep 25 for content collections

--- .astro/types.d.ts
+++ .astro/types.d.ts
@@ -186,6 +186,13 @@ declare module 'astro:content' {
   collection: "episodes",
   data: InferEntrySchema<"episodes">
 },
+"25.md": {
+  id: "25.md",
+  slug: "25",
+  body: string,
+  collection: "episodes",
+  data: InferEntrySchema<"episodes">
+},
 "3.md": {
   id: "3.md",
   slug: "3",
