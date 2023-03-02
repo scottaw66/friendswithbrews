563ba780293b9dec5af5f07363a8783b9f196bef
Scott Willsey <sunny.flame0794@fastmail.com>
2023-03-02 07:18:37 -0800

    Episode 26

--- .astro/types.d.ts
+++ .astro/types.d.ts
@@ -193,6 +193,13 @@ declare module 'astro:content' {
   collection: "episodes",
   data: InferEntrySchema<"episodes">
 },
+"26.md": {
+  id: "26.md",
+  slug: "26",
+  body: string,
+  collection: "episodes",
+  data: InferEntrySchema<"episodes">
+},
 "3.md": {
   id: "3.md",
   slug: "3",
