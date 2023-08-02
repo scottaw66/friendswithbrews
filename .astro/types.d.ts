07193dce8b5b5f17f3ff4e2dec822e50cd924993
Scott Willsey <sunny.flame0794@fastmail.com>
2023-08-01 22:47:12 -0700

    Adds episode 46

--- .astro/types.d.ts
+++ .astro/types.d.ts
@@ -469,6 +469,13 @@ declare module 'astro:content' {
   collection: "episodes";
   data: InferEntrySchema<"episodes">
 } & { render(): Render[".md"] };
+"46.md": {
+	id: "46.md";
+  slug: "46";
+  body: string;
+  collection: "episodes";
+  data: InferEntrySchema<"episodes">
+} & { render(): Render[".md"] };
 "5.md": {
 	id: "5.md";
   slug: "5";
