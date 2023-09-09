a93edb912b8d82cd8d5ecd7d2420f8ad8a8fdfd3
Scott Willsey <sunny.flame0794@fastmail.com>
2023-09-08 20:32:14 -0700

    Continues Astro 3.0 migration with assets folder and changing pages to use Image from astro:assets, as well as gets rid of config package for site data in favor of src/data/site.json file

--- src/pages/feed.xml.js
+++ src/pages/feed.xml.js
@@ -1,23 +1,22 @@
 import rss from "@astrojs/rss";
 import sanitizeHtml from "sanitize-html";
-import config from "config";
 import { rfc2822, year } from "../components/utilities/DateFormat";
+import site from "../data/site.json";
 
-const episodeImportResult = import.meta.glob("../content/episodes/*.md", {
-  eager: true,
-});
-let episodes = Object.values(episodeImportResult);
-episodes = episodes.sort(
-  (a, b) =>
-    new Date(b.frontmatter.date).valueOf() -
-    new Date(a.frontmatter.date).valueOf()
-);
-
-export const get = () =>
-  rss({
-    title: config.get("title"),
-    description: config.get("description"),
-    site: config.get("url"),
+export function GET(context) {
+  const episodeImportResult = import.meta.glob("../content/episodes/*.md", {
+    eager: true,
+  });
+  let episodes = Object.values(episodeImportResult);
+  episodes = episodes.sort(
+    (a, b) =>
+      new Date(b.frontmatter.date).valueOf() -
+      new Date(a.frontmatter.date).valueOf()
+  );
+  return rss({
+    title: site.title,
+    description: site.description,
+    site: site.url,
     xmlns: {
       atom: "http://www.w3.org/2005/Atom/",
       dc: "http://purl.org/dc/elements/1.1/",
@@ -26,43 +25,39 @@ export const get = () =>
     },
     customData: `
     <language>en-us</language>
-    <itunes:title>${config.get("title")}</itunes:title>
+    <itunes:title>${site.title}</itunes:title>
     <itunes:author>Beeping Machine</itunes:author>
     <itunes:owner>
-      <itunes:name>${config.get("name")}</itunes:name>
-      <itunes:email>${config.get("email")}</itunes:email>
+      <itunes:name>${site.name}</itunes:name>
+      <itunes:email>${site.email}</itunes:email>
     </itunes:owner>
     <itunes:type>episodic</itunes:type>
     <itunes:category text="Technology">
       <itunes:category text="Society &amp; Culture"/>
     </itunes:category>
     <itunes:keywords>Technology, Fitness, Beer</itunes:keywords>
-    <itunes:image href="${
-      config.get("url") + "images/" + config.get("rss.image")
-    }"/>
+    <itunes:image href="${site.url}/images/${site.rss.image}" />
     <itunes:explicit>No</itunes:explicit>
-    <itunes:new-feed-url>${
-      config.get("url") + config.get("rss.fileName")
-    }</itunes:new-feed-url>
+    <itunes:new-feed-url>${site.url}${site.rss.fileName}</itunes:new-feed-url>
     <image>
-    <url>${config.get("url") + "images/" + config.get("rss.image")}</url>
-    <title>${config.get("title")}</title>
-    <link>${config.get("url")}</link>
+    <url>${site.url}images/${site.rss.image}</url>
+    <title>${site.title}</title>
+    <link>${site.url}</link>
     </image>
-    <copyright>©${year()} ${config.get("name")}</copyright>`,
+    <copyright>©${year()} ${site.name}</copyright>`,
     items: Array.from(episodes).map((episode) => ({
       title: episode.frontmatter.title,
-      link: `${config.get("url")}${episode.frontmatter.slug}`,
+      link: `${site.url}${episode.frontmatter.slug}`,
       pubDate: rfc2822(episode.frontmatter.date),
       description: episode.frontmatter.description,
       content: sanitizeHtml(episode.compiledContent()),
-      customData: `<enclosure url="${config.get("episodes.audioPrefix")}${
+      customData: `<enclosure url="${site.episodes.audioPrefix}${
         episode.frontmatter.audioFile
       }" length="${episode.frontmatter.bytes}" type="audio/mpeg" />
       <itunes:title>${episode.frontmatter.title}</itunes:title>
       <itunes:episode>${episode.frontmatter.episode}</itunes:episode>
       <itunes:duration>${episode.frontmatter.length}</itunes:duration>
-      <itunes:image href="${config.get("url")}images/${config.get(
+      <itunes:image href="${site.url}images/${config.get(
         "rss.image"
       )}"/>
       <itunes:explicit>No</itunes:explicit>
@@ -70,5 +65,7 @@ export const get = () =>
       <itunes:summary><![CDATA[${
         episode.frontmatter.description
       }]]></itunes:summary>`,
+      ...post.frontmatter,
     })),
   });
+}
