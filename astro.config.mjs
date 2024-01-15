0336fc7b1ab2bdbfe014b09b9e1b000a74704e49
Scott Willsey <sunny.flame0794@fastmail.com>
2024-01-14 16:06:04 -0800

    Upgrades to astro-icon 1.x, lots of supporting tweaks

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from "astro/config";
+import icon from "astro-icon";
 import pagefind from "astro-pagefind";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://friendswithbeer.com/",
-  integrations: [ pagefind(), sitemap(),],
+  integrations: [ icon(), pagefind(), sitemap(),],
 });
