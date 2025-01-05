702c7cb2d97bde7591aa2b353226cbf19b0ecd52
Scott Willsey <sunny.flame0794@fastmail.com>
2025-01-04 23:15:09 -0800

    Updates transcript content config

--- src/content.config.ts
+++ src/content.config.ts
@@ -23,6 +23,7 @@ export const collections = {
       date: z.string().transform((str) => new Date(str)),
       audioFile: z.string(),
       length: z.string().regex(/(\d{2}:){2}\d{2}/),
+      bytes: z.string().optional(),
     }),
   }),
 };
