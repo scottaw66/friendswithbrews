84f1cf01d2de49cb09682def2275847aab16d8f0
Scott Willsey <firm.vest0287@fastmail.com>
2026-06-21 12:26:09 -0700

    Caching fix (hopefully)

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://friendswithbeer.com/",
+  site: "https://friendswithbrews.com/",
   integrations: [ icon(), pagefind(), sitemap(),],
 });
